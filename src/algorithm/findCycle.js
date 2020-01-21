2bc8a644f97d944ffb847d58528c61d41c6bd04e
Bartłomiej Rutkowski <bartek@ethworks.io>
2020-01-21 21:22:51 +0100

    Add example; fix iteration issue

--- src/algorithm/findCycle.js
+++ src/algorithm/findCycle.js
@@ -4,12 +4,17 @@ function findCycle (graph, currentNode, alreadySeen) {
   if(alreadySeen.includes(currentNode)) {
     return formatCycle([...alreadySeen, currentNode].slice(alreadySeen.indexOf(currentNode)))
   }
-  updatedAlreadySeen = [...alreadySeen, currentNode]
+  const updatedAlreadySeen = [...alreadySeen, currentNode];
   if (graph[currentNode] !== undefined) {
     const nodes = Object.keys(graph[currentNode])
-    for (i = 0; i < nodes.length; i++) {
-      return findCycle(graph, nodes[i], updatedAlreadySeen)
+    let result;
+    for (let i = 0; i < nodes.length; i++) {
+      result = findCycle(graph, nodes[i], updatedAlreadySeen)
+      if(result !== undefined) {
+        break
+      }
     }
+    return result
   }
 }
 
